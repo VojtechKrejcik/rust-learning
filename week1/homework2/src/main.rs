e0220e7ee7ca5514904bdee25ebe1fabae5777c7
Vojtěch Krejčík <vojtech.krejcik@gmail.com>
2023-10-08 13:52:21 +0200

    formated and tested

--- week1/homework2/src/main.rs
+++ week1/homework2/src/main.rs
@@ -1,32 +1,30 @@
-use::slug::slugify;
-use std::io;
+use ::slug::slugify;
 use std::env;
-
+use std::io;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1{
-        println!("Please specify one of the following operations as argument:
+    if args.len() == 1 {
+        println!(
+            "Please specify one of the following operations as argument:
     -lowercase
     -uppercase
     -no-spaces
-    -slugify");
-    return Ok(());
+    -slugify"
+        );
+        return Ok(());
     };
     let mut user_input = String::new();
-    let stdin = io::stdin(); 
+    let stdin = io::stdin();
     let _ = stdin.read_line(&mut user_input);
 
-    match args[1].as_str(){
-        "lowercase" => println!("{}",user_input.to_lowercase()),
-        "uppercase" => println!("{}",user_input.to_uppercase()),
-        "no-spaces" => println!("{}",user_input.replace(" ", "")),
-        "slugify" => println!("{}",slugify(user_input)),
-        _ => println!("Unsupported operation")
+    match args[1].as_str() {
+        "lowercase" => println!("{}", user_input.to_lowercase()),
+        "uppercase" => println!("{}", user_input.to_uppercase()),
+        "no-spaces" => println!("{}", user_input.replace(" ", "")),
+        "slugify" => println!("{}", slugify(user_input)),
+        _ => println!("{} is unsupported operation", args[1]),
+    }
 
-   }
-
-
- 
     Ok(())
 }
